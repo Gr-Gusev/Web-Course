92a1b55f27cdfad4d639ef5d7466cd37446a3f62
g_gusev <gr@medhim.ru>
2021-02-24 22:18:11 +0300

    Stage 1

---
+++ create_tables.sql
@@ -0,0 +1,38 @@
+CREATE TABLE students (
+	student_id serial PRIMARY KEY,
+	student_name varchar(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE companies (
+	company_id serial PRIMARY KEY UNIQUE,
+	company_name varchar(50) NOT NULL,
+	address varchar(100) NOT NULL
+);
+
+CREATE TABLE courses (
+	course_id serial PRIMARY KEY,
+	company_id integer REFERENCES companies (company_id) NOT NULL,
+	date_of_start date NOT NULL,
+	duration interval NOT NULL,
+	hours_per_day smallint,
+	course_name varchar(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE teachers (
+	teacher_id serial PRIMARY KEY,
+	teacher_name varchar(100) NOT NULL UNIQUE,
+	company_id integer REFERENCES companies (company_id)
+);
+
+CREATE TABLE classes (
+	class_id serial PRIMARY KEY,
+	course_id integer REFERENCES courses (course_id) NOT NULL,
+	teacher_id integer REFERENCES teachers (teacher_id) NOT NULL,
+	date_and_time timestamp NOT NULL
+);
+
+CREATE TABLE student_courses (
+	student_id integer REFERENCES students (student_id) NOT NULL,
+	course_id integer REFERENCES courses (course_id) NOT NULL,
+	PRIMARY KEY (student_id, course_id)
+);
---
+++ drop_tables.sql
@@ -0,0 +1,11 @@
+DROP TABLE student_courses;
+
+DROP TABLE classes;
+
+DROP TABLE teachers;
+
+DROP TABLE courses;
+
+DROP TABLE companies;
+
+DROP TABLE students;
